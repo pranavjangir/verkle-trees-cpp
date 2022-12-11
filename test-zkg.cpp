2e2fe14d702cbb859392ed254c8296f462110e61
Pranav Jangir <jangir.pranav@gmail.com>
2022-12-11 00:33:55 -0500

    Remove some binaries and fix the C library issue.

--- test-zkg.cpp
+++ test-zkg.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
-#include "blst.h"
+extern "C" {
+  #include "blst.h"
+  #include "c_kzg.h"
+  #include "bls12_381.h"
+}
 //#include "gmpxx.h"
-#include "c_kzg.h"
-#include "bls12_381.h"
 
 using namespace std;
 
@@ -10,8 +12,9 @@ int main() {
     // Create a polynomial and ask for proof from the zkg library!
     cout << "Hello!!!\n";
     FFTSettings fft;
-    unsigned int max_scale = (1 << 10);
-    new_fft_settings(&fft, max_scale);
+    unsigned int max_scale = 10;
+    assert(C_KZG_OK == new_fft_settings(&fft, max_scale));
+    // new_fft_settings(&fft, max_scale);
     cout << fft.max_width << endl;
     free_fft_settings(&fft);
 }
