ec7c90654cf99d6d1ddbeec8ffff8697a3c6dbb8
Arun Patro <arun.kumar.patro@gmail.com>
2022-12-15 00:55:52 -0500

    update

--- arun-js/start.js
+++ arun-js/start.js
@@ -17,13 +17,6 @@ async function test2() {
   await trie.put(Buffer.from('test'), Buffer.from('one'))
   await trie.put(Buffer.from('test1'), Buffer.from('two'))
   await trie.put(Buffer.from('test2'), Buffer.from('three'))
-  await trie.put(Buffer.from('test3'), Buffer.from('four'))
-  await trie.put(Buffer.from('test4'), Buffer.from('four'))
-  await trie.put(Buffer.from('test5'), Buffer.from('four'))
-  await trie.put(Buffer.from('test6'), Buffer.from('four'))
-  await trie.put(Buffer.from('test7'), Buffer.from('four'))
-  await trie.put(Buffer.from('test8'), Buffer.from('four'))
-  await trie.put(Buffer.from('test9'), Buffer.from('four'))
   const proof = await Trie.createProof(trie, Buffer.from('test'))
   const value = await Trie.verifyProof(trie.root, Buffer.from('test'), proof)
   console.log("proof is ", proof)
@@ -33,4 +26,77 @@ async function test2() {
   console.log(roughObjSize)
 }
 
-test2()
+// test2()
+// const fs = require("fs");
+import fs from "fs";
+import { assert } from 'console'
+var data = JSON.parse(fs.readFileSync('./data1.json', 'utf8'));
+
+// const data = require('./data1.json');
+
+async function test3() {
+
+    for (var blockNumber in data) {
+      // if (blockNumber == "f6edc6") {
+      //   break
+      // }
+      for (var idx in data[blockNumber]) {
+        // if (idx == 20) {
+        //   break
+        // }
+        var address = data[blockNumber][idx]
+        var randValue = parseInt(Math.random() * 1000).toString();
+        console.log("[state-change item]", blockNumber, idx, address, randValue)
+
+        // insert into trie
+        await trie.put(Buffer.from(address), Buffer.from(randValue))
+
+        // fetch from trie
+        const fetchValue = await trie.get(Buffer.from(address))
+
+        // verify with assert
+        console.log("[fetch & verify]", blockNumber, idx, address, fetchValue.toString(), randValue)
+        assert(fetchValue == randValue)
+
+        // // get proof and verify
+        // var proof = await Trie.createProof(trie, Buffer.from(address))
+        // var proofValue = await Trie.verifyProof(trie.root, Buffer.from(address), proof)
+        // console.log("proof", address, proofValue.toString(), randValue)
+
+      }
+      console.log(blockNumber, JSON.stringify(trie).length);
+    }
+
+    // const roughObjSize = JSON.stringify(trie).length;
+    console.log("done");
+
+    // return trie
+
+}
+
+async function test4() {
+  for (var blockNumber in data) {
+    // if (blockNumber == "f6edc6") {
+    //   break
+    // }
+    for (var idx in data[blockNumber]) {
+      // if (idx == 20) {
+      //   break
+      // }
+      var address = data[blockNumber][idx]
+      let fetchValue = await trie.get(Buffer.from(address))
+
+      // console.log("proof", blockNumber, address, proofValue.toString())
+      
+      let proof = await Trie.createProof(trie, Buffer.from(address))
+      let proofValue = await Trie.verifyProof(trie.root, Buffer.from(address), proof)
+
+      console.log("[proof]", blockNumber, idx, address, proof.length, fetchValue.toString(), proofValue.toString())
+    }
+  }
+}
+console.time('test');
+await test3()
+await test4()
+console.log("Trie now at ", JSON.stringify(trie).length);
+console.timeEnd('test');
